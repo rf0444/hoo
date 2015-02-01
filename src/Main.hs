c925be21c430f52d08a29e8f2d90128e45e46b18
rf <fujisawa_r@welmo.co.jp>
2015-02-01 15:48:26 +0900

    hello process

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Control.Distributed.Process (Process, liftIO)
+import Control.Distributed.Process.Node (runProcess, initRemoteTable, newLocalNode)
+import Network.Socket (withSocketsDo)
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
 main :: IO ()
-main = putStrLn "Hello"
+main = do
+  et <- withSocketsDo $ createTransport "localhost" "10501" defaultTCPParameters
+  case et of
+    Left e -> print e
+    Right t -> do
+      node <- newLocalNode t initRemoteTable
+      runProcess node mainProcess
+
+mainProcess :: Process ()
+mainProcess = do
+  liftIO $ putStrLn "hello"
